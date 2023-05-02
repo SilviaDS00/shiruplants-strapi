65e49b746c393c1e56c5921e69eb47cf0db5112c
Silvia Donaire <alumno.1360752@ies-azarquiel.es>
2023-05-02 15:47:58 +0200

    feat: added collections types

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,6 +1,5 @@
 module.exports = [
   'strapi::errors',
-  'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
@@ -9,4 +8,31 @@ module.exports = [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
-];
+    {
+      name: 'strapi::security',
+      config: {
+        contentSecurityPolicy: {
+          useDefaults: true,
+          directives: {
+            'connect-src': ["'self'", 'https:'],
+            'img-src': [
+              "'self'",
+              'data:',
+              'blob:',
+              'dl.airtable.com',
+              'https://shiruplants.s3.eu-west-3.amazonaws.com/',
+            ],
+            'media-src': [
+              "'self'",
+              'data:',
+              'blob:',
+              'dl.airtable.com',
+              'https://shiruplants.s3.eu-west-3.amazonaws.com/',
+            ],
+            upgradeInsecureRequests: null,
+          },
+        },
+      },
+    },
+  ];
+
